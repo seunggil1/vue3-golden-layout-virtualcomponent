b44aaeadfb99860d5f2dc3a7fdffd0f7ed4b6ecb
seung <38664481+seunggil1@users.noreply.github.com>
2022-07-29 22:33:26 +0900

    add build library script

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,10 @@
-import { createApp } from 'vue'
+import GlLayout from './components/GlLayout.vue';
+import GlComponent from './components/GlComponent.vue';
+import ContentFirst from './components/ContentFirst.vue';
+import ContentSecond from './components/ContentSecond.vue';
+import ContentThird from './components/ContentThird.vue';
 import GlMain from './GlMain.vue';
 
-createApp(GlMain).mount('#app')
+import { prefinedLayouts } from "./ts/predefined-layouts";
+
+export { GlLayout, GlComponent, ContentFirst, ContentSecond, ContentThird, GlMain, prefinedLayouts}
